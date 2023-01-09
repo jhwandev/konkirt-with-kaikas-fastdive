0622748e9dda47e85be908bdee70d5cc0b8819d2
jhwandev <jhwan.dev@gmail.com>
2023-01-09 18:59:30 +0900

    기능추가 - 내가 보유한 NFT 확인

---
+++ src/components/molecules/MyItem.jsx
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+import * as colors from "@styles/colors";
+
+const CardWrapper = styled.div`
+  border-radius: 16px;
+  flex-shrink: 0;
+  overflow: hidden;
+`;
+
+const CardImage = styled.img`
+  width: 150px;
+  height: 150px;
+  object-fit: contain;
+  vertical-align: middle;
+`;
+
+const InfoBox = styled.div`
+  width: 150px;
+  height: 73px;
+  padding: 16px;
+  background-color: ${colors.bgSecondary};
+`;
+
+const CollectionTitle = styled.div`
+  font-size: 15px;
+  color: ${colors.textYellow};
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+
+const Title = styled.div`
+  font-family: MarkPro-Heavy;
+  font-size: 14px;
+  margin-top: 4px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
+export default function MyItem({ item }) {
+  console.log(item);
+  return (
+    <CardWrapper>
+      <CardImage src={item.image}></CardImage>
+      <InfoBox>
+        <CollectionTitle>{item.attributes[0].value}</CollectionTitle>
+        <Title>{item.name}</Title>
+      </InfoBox>
+    </CardWrapper>
+  );
+}
---
+++ src/components/templates/MyNfts.jsx
@@ -0,0 +1,15 @@
+import SectionLayout from "@components/molecules/SectionLayout";
+import SectionTop from "@components/molecules/SectionTop";
+import MyItems from "@components/organisms/MyItems";
+
+function MyNfts({ user }) {
+  console.log(user);
+  return (
+    <SectionLayout>
+      <SectionTop title="내가 보유한 NFT" showAll="아이템 전체보기" />
+      <MyItems metadata={user.result} />
+    </SectionLayout>
+  );
+}
+
+export default MyNfts;
